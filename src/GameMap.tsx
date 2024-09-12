d774d664e145efc8889c115f53fb6409cb5164fa
Vojtěch Brůža <vojtech.bruza@gmail.com>
2024-09-12 09:45:04 +0200

    Separate the MapMask component

--- src/GameMap.tsx
+++ src/GameMap.tsx
@@ -1,10 +1,11 @@
 import { useState, useRef } from "react";
 import { MapContainer, Marker, Pane, Polygon, Popup, TileLayer, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L, { LatLngExpression } from "leaflet";
+import L, { Bounds, LatLngExpression } from "leaflet";
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import MapMask from "./MapMask";
 
 let DefaultIcon = L.icon({
     iconSize: [25, 41],
@@ -15,42 +16,6 @@ let DefaultIcon = L.icon({
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
-
-// map bounds (display a "background" around this area)
-const bounds = L.latLngBounds(
-    L.latLng(53.261616, -6.496525), // Southwest corner
-    L.latLng(53.40118, -6.03441)  // Northeast corner
-);
-
-const MapMask = () => {
-    const mapCorners: LatLngExpression[] = [
-        [90, -180], // Top-left
-        [90, 180],  // Top-right
-        [-90, 180], // Bottom-right
-        [-90, -180] // Bottom-left
-    ];
-
-    const boundsCoords: LatLngExpression[] = [
-        [bounds.getSouthWest().lat, bounds.getSouthWest().lng],
-        [bounds.getSouthWest().lat, bounds.getNorthEast().lng],
-        [bounds.getNorthEast().lat, bounds.getNorthEast().lng],
-        [bounds.getNorthEast().lat, bounds.getSouthWest().lng],
-    ];
-
-    const maskCoords: LatLngExpression[][] = [
-        [...mapCorners],
-        [...boundsCoords]
-    ];
-
-    return (
-        <Pane name="mask-pane" style={{ zIndex: 500 }}>
-            <Polygon
-                positions={maskCoords}
-                pathOptions={{ color: 'black', fillColor: 'black', fillOpacity: 0.7, stroke: false }}
-            />
-        </Pane>
-    );
-};
 
 // Component to handle clicks and add markers
 function ClickHandler({ onClick }: { onClick: (latlng: LatLngExpression) => void }) {
@@ -63,7 +28,14 @@ function ClickHandler({ onClick }: { onClick: (latlng: LatLngExpression) => void
 }
 
 function GameMap() {
+    // map bounds (display a "background" around this area)
+    const bounds = L.latLngBounds(
+        L.latLng(53.261616, -6.496525), // Southwest corner
+        L.latLng(53.40118, -6.03441)  // Northeast corner
+    );
+
     const gameMapRef = useRef(null);
+    // stores positions of all markers, use setMarkers to set it and notify React to rerender the page
     const [markers, setMarkers] = useState<LatLngExpression[]>([]);
 
     const addMarker = (position: LatLngExpression) => {
@@ -73,7 +45,7 @@ function GameMap() {
     return (
         <MapContainer
             center={[53.3484000, -6.2539000]}
-            zoom={13} 
+            zoom={13}
             scrollWheelZoom={true}
             ref={gameMapRef}
             style={{ height: "85vh", width: "90vw", margin: "auto" }}
@@ -94,7 +66,7 @@ function GameMap() {
                 </Marker>
             ))}
 
-            <MapMask />
+            <MapMask bounds={bounds}/>
         </MapContainer>
     );
 }
---
+++ src/MapMask.tsx
@@ -0,0 +1,38 @@
+import { LatLngBounds, LatLngExpression } from "leaflet";
+import { Pane, Polygon } from "react-leaflet";
+
+interface MapMaskProps {
+    bounds: LatLngBounds;
+}
+
+const MapMask = ({ bounds }: MapMaskProps) => {
+    const mapCorners: LatLngExpression[] = [
+        [90, -180], // Top-left
+        [90, 180],  // Top-right
+        [-90, 180], // Bottom-right
+        [-90, -180] // Bottom-left
+    ];
+
+    const boundsCoords: LatLngExpression[] = [
+        [bounds.getSouthWest().lat, bounds.getSouthWest().lng],
+        [bounds.getSouthWest().lat, bounds.getNorthEast().lng],
+        [bounds.getNorthEast().lat, bounds.getNorthEast().lng],
+        [bounds.getNorthEast().lat, bounds.getSouthWest().lng],
+    ];
+
+    const maskCoords: LatLngExpression[][] = [
+        [...mapCorners],
+        [...boundsCoords]
+    ];
+
+    return (
+        <Pane name="mask-pane" style={{ zIndex: 500 }}>
+            <Polygon
+                positions={maskCoords}
+                pathOptions={{ color: 'black', fillColor: 'black', fillOpacity: 0.7, stroke: false }}
+            />
+        </Pane>
+    );
+};
+
+export default MapMask;
