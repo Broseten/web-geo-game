d00cc29b2a5a3b70920ae6eb70d59c3c0e1f3d2f
Vojtěch Brůža <vojtech.bruza@gmail.com>
2024-09-26 11:26:16 +0200

    Added socket IO with a simple connection test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-import { Box, Center, Heading } from '@chakra-ui/react'
-import GameMap from './Components/Map/GameMap'
+import { Box, Button, Center, Heading, Text } from '@chakra-ui/react'
+import { socket } from './main'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [testCounter, setTestCounter] = useState(0);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onTestClient() {
+      // must use a function -- because javscript closures
+      // since the counter is a const, it captures its value when creating this callback...
+      setTestCounter(testCounter => testCounter + 1);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('testClient', onTestClient);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('testClient', onTestClient);
+    };
+  }, []);
+
   return (
     <>
       <Box className="app" backgroundColor="gray.100" h='calc(100vh)'>
         <Center>
           <Heading mb={4}>The Game</Heading>
         </Center>
-        <GameMap />
+        <Center>
+          <Text>{isConnected ? "connected" : "not connected"}, {`triggered ${testCounter} times`}</Text>
+          <Button onClick={() => {
+            socket.emit("test");
+          }}>Click me</Button>
+          {/* <GameMap /> */}
+        </Center>
       </Box>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import customTheme from './Theme.ts'
+import { io } from 'socket.io-client';
 
-// TODO connection etc.
+const URL = 'http://localhost:1337';
+
+export const socket = io(URL, {
+  autoConnect: true
+});
 
 createRoot(document.getElementById('root')!).render(
   //<StrictMode>
